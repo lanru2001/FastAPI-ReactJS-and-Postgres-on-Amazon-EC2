6664353ee9e5d8a3e6d0c0a23e2f231804f71ea7
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2026-07-01 13:17:02 -0500

    Create main.tf

---
+++ todo-app/infra/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Security Group
+resource "aws_security_group" "todo_sg" {
+  name        = "todo-app-sg"
+  description = "Allow HTTP, SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "todo_ec2" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name              = var.key_name
+  vpc_security_group_ids = [aws_security_group.todo_sg.id]
+
+  user_data = file("${path.module}/user_data.sh")
+
+  tags = {
+    Name = "todo-app-server"
+  }
+}
